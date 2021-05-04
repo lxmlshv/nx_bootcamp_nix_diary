3a809965b99960c4b948454cf781f6306c274707
Malyshev Alexey <maddrone@ya.ru>
2021-05-04 15:54:20 +0300

    Команда backup

--- diary.sh
+++ diary.sh
@@ -225,7 +225,12 @@ fi
 
 if [[ $1 == backup ]]
 then
-	echo "Команда backup"
+	shift
+	if (( $# == 0)); then
+		echo "Укажите имя архива"
+		return
+	fi
+	tar -cvzf "$1.tar.gz" -C "$DIARY_PATH/../" $(basename $DIARY_PATH)
 	return
 fi
 
--- src/diary_backup.sh
+++ src/diary_backup.sh
@@ -6,6 +6,11 @@
 
 if [[ $1 == backup ]]
 then
-	echo "Команда backup"
+	shift
+	if (( $# == 0)); then
+		echo "Укажите имя архива"
+		return
+	fi
+	tar -cvzf "$1.tar.gz" -C "$DIARY_PATH/../" $(basename $DIARY_PATH)
 	return
 fi
