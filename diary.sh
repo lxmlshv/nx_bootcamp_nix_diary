e0525524e0c396bff6197d690bcad721ffcfad7d
Malyshev Alexey <maddrone@ya.ru>
2021-05-04 16:44:23 +0300

    Stats update

--- diary.sh
+++ diary.sh
@@ -173,7 +173,8 @@ fi
 
 if [[ $1 == stats ]]
 then
-	echo "Записей в дневнике" $(ls -1q "$DIARY_PATH"/*/*/*.md | wc -l)
+	echo "Записей в дневнике:" $(ls -1q "$DIARY_PATH"/*/*/*.md | wc -l)
+	echo "Последняя запись сделана:" $(ls -rt $DIARY_PATH/*/*/*.md | tail -n1 | grep -Po "(?<=__)[^/_]*")
 	return
 fi
 
--- src/diary_stats.sh
+++ src/diary_stats.sh
@@ -7,5 +7,6 @@
 if [[ $1 == stats ]]
 then
 	echo "Записей в дневнике:" $(ls -1q "$DIARY_PATH"/*/*/*.md | wc -l)
+	echo "Последняя запись сделана:" $(ls -rt $DIARY_PATH/*/*/*.md | tail -n1 | grep -Po "(?<=__)[^/_]*")
 	return
 fi
